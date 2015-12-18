132cbae5cb78c2730ee40d9bf9694ffa286c9cd2
Georgy Vlasov <wlasowegor@gmail.com>
2015-12-18 16:28:42 +0500

    Move files related to constructing a RandomlyCutPolygon to a separate package

--- src/main/kotlin/org/tendiwa/plane/geometry/algorithms/polygons/RandomlyCutPolygonDemo.kt
+++ src/main/kotlin/org/tendiwa/plane/geometry/algorithms/polygons/RandomlyCutPolygonDemo.kt
@@ -3,7 +3,7 @@ package org.tendiwa.plane.geometry.algorithms.polygons
 import org.tendiwa.canvas.algorithms.geometry.draw
 import org.tendiwa.canvas.awt.AwtCanvas
 import org.tendiwa.plane.directions.OrdinalDirection.*
-import org.tendiwa.plane.geometry.algorithms.polygons.cut.cutRandomly
+import org.tendiwa.plane.geometry.algorithms.polygons.cut.random.cutRandomly
 import org.tendiwa.plane.geometry.algorithms.polygons.cut.cuts
 import org.tendiwa.plane.geometry.circles.Circle
 import org.tendiwa.plane.geometry.points.Point
