c95ab34a3342fc860b329dec08619212a2d75ea5
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-27 05:52:36 +0500

    Add a demo for A* pathfinding

---
+++ src/main/kotlin/org/tendiwa/plane/grid/pathfinding/AstarDemo.kt
@@ -0,0 +1,25 @@
+package org.tendiwa.plane.grid.pathfinding
+
+import org.tendiwa.canvas.algorithms.grid.draw
+import org.tendiwa.canvas.awt.AwtCanvas
+import org.tendiwa.plane.grid.algorithms.pathfinding.astar.path
+import org.tendiwa.plane.grid.constructors.GridRectangle
+import org.tendiwa.plane.grid.dimensions.by
+import org.tendiwa.plane.grid.masks.difference
+import org.tendiwa.plane.grid.tiles.Tile
+import java.awt.Color
+
+fun main(args: Array<String>) {
+    val viewport = GridRectangle(120 by 120)
+    val passableMask = GridRectangle(100 by 100)
+        .difference(GridRectangle(Tile(10, 30), 80 by 20))
+    val path = passableMask.path(
+        start = Tile(40, 5),
+        end = Tile(50, 90)
+    )
+    AwtCanvas(viewport).apply {
+        draw(passableMask, Color.green)
+        draw(path, Color.red)
+    }
+}
+
