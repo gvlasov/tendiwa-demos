1af4a448856d1de97d5fc7f74ef17154c5697071
Georgy Vlasov <wlasowegor@gmail.com>
2016-02-02 07:33:01 +0500

    Rewrite StainDemo to use animations

--- src/main/kotlin/org/tendiwa/plane/grid/stains/StainDemo.kt
+++ src/main/kotlin/org/tendiwa/plane/grid/stains/StainDemo.kt
@@ -2,22 +2,48 @@ package org.tendiwa.plane.grid.stains
 
 import org.tendiwa.canvas.algorithms.grid.drawGridMask
 import org.tendiwa.canvas.awt.AwtCanvas
+import org.tendiwa.canvas.awt.gif.AnimationFrame
+import org.tendiwa.canvas.awt.gif.Fps
+import org.tendiwa.canvas.awt.gif.Gif
+import org.tendiwa.canvas.awt.gif.showInBrowser
 import org.tendiwa.math.noise.PerlinNoise
+import org.tendiwa.math.noise.move
 import org.tendiwa.math.noise.scale
+import org.tendiwa.plane.grid.algorithms.buffers.intrudedContour
 import org.tendiwa.plane.grid.algorithms.stains.Stain
+import org.tendiwa.plane.grid.constructors.GridRectangle
 import org.tendiwa.plane.grid.dimensions.by
+import org.tendiwa.plane.grid.masks.boundedBy
+import org.tendiwa.plane.grid.masks.inverse
 import org.tendiwa.plane.grid.tiles.Tile
 import java.awt.Color
 
 fun main(args: Array<String>) {
     val size = 150
-    val stain = Stain(
-        start = Tile(size, size),
-        noise = PerlinNoise(octave = 7).scale(1.0 / 40.0),
-        size = size
+    val frames = 80
+    val viewport = GridRectangle(size * 2 by size * 2)
+    Gif(
+        canvas = AwtCanvas(viewport, scale = 1),
+        frames = (0..frames)
+            .map { index ->
+                AnimationFrame {
+                    val stain = Stain(
+                        start = Tile(size, size),
+                        noise = PerlinNoise(octave = 7)
+                            .scale(1.0 / 64 + 1 / 200 * index)
+                            .move(
+                                40 * Math.sin(Math.PI * 2 / frames * index),
+                                40 * Math.cos(Math.PI * 2 / frames * index)
+                            ),
+                        size = size
+                    )
+                    val contour = stain.inverse
+                        .intrudedContour(4, 1)
+                        .boundedBy(viewport)
+                    drawGridMask(stain, Color.red)
+                    drawGridMask(contour, Color.blue)
+                }
+            }
     )
-    AwtCanvas(size * 2 by size * 2, scale = 1)
-        .apply {
-            drawGridMask(stain, Color.red)
-        }
+        .showInBrowser(Fps.MAX)
 }
