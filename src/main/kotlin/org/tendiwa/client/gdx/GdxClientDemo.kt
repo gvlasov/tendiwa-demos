87d30c9f2332bc7757fc75c20362a501bb2ac28d
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-04 06:14:05 +0500

    Extract the code that is about to be changed from TendiwaGame

--- src/main/kotlin/org/tendiwa/client/gdx/GdxClientDemo.kt
+++ src/main/kotlin/org/tendiwa/client/gdx/GdxClientDemo.kt
@@ -2,6 +2,8 @@ package org.tendiwa.client.gdx
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import org.tendiwa.client.gdx.temporaryImpls.CameraInputAdapter
+import org.tendiwa.client.gdx.temporaryImpls.ExampleVicinity
 
 fun main(args: Array<String>) {
     val config =
@@ -11,9 +13,12 @@ fun main(args: Array<String>) {
             height = 480
             resizable = false
         }
+    val vicinity = ExampleVicinity()
     LwjglApplication(
         TendiwaGame(
-            "atlas/example.atlas"
+            atlasPath = "atlas/example.atlas",
+            createInputProcessor = { camera -> CameraInputAdapter(camera, vicinity) },
+            vicinity = vicinity
         ),
         config
     )
